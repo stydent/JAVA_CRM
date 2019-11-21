c62805cc64d3db2824bd8d29139075c9fe9583ec
Stydent <akuritsyn@yandex.ru>
2019-11-21 22:52:58 +0300

    to normalize the structure

--- src/main/java/ru/vladlink/crm/aplication/CrmApplication.java
+++ src/main/java/ru/vladlink/crm/aplication/CrmApplication.java
@@ -2,14 +2,12 @@ package ru.vladlink.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={"ru.vladlink.crm.repository"})
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={"ru.vladlink.crm.repository"})
+
+//@SpringBootApplication
 public class CrmApplication {
 
     public static void main(String[] args) {
