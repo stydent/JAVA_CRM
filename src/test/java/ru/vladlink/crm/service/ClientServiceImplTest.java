5c91b903e7590d9c56d2252a331bee1b1827c009
Stydent <akuritsyn@yandex.ru>
2019-12-11 23:36:24 +0300

    add test client remove

---
+++ src/test/java/ru/vladlink/crm/service/ClientServiceImplTest.java
@@ -0,0 +1,23 @@
+package ru.vladlink.crm.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.vladlink.crm.entity.Client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ClientServiceImplTest {
+
+    @Autowired
+    private ClientService clientService;
+    @Test
+    void removeClient() {
+        Client client = clientService.getOne(10);
+        clientService.removeClient(client.getId());
+        Assert.assertTrue(client.status==0);
+    }
+}
